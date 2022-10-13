758d1279ad6db223d43a1d63a0fe6371d6041d4b
Marco Lewandowski <lewandowski@swms.de>
2022-10-13 10:00:01 +0200

    bugfix x# syntax

--- Source/Logging/MqttNetLoggerWrapper.cs
+++ Source/Logging/MqttNetLoggerWrapper.cs
@@ -3,58 +3,59 @@ using System.Threading;
 using Microsoft.Extensions.Logging;
 using MQTTnet.Diagnostics.Logger;
 
-namespace MQTTnet.Server.Logging;
-
-public sealed class MqttNetLoggerWrapper : IMqttNetLogger
+namespace MQTTnet.Server.Logging
 {
-    private readonly ILogger<MqttServer> _logger;
-
-    public MqttNetLoggerWrapper(ILogger<MqttServer> logger)
+    public sealed class MqttNetLoggerWrapper : IMqttNetLogger
     {
-        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-    }
+        private readonly ILogger<MqttServer> _logger;
 
-    public void Publish(MqttNetLogLevel level, string source, string message, object[] parameters, Exception exception)
-    {
-        var convertedLogLevel = ConvertLogLevel(level);
-        _logger.Log(convertedLogLevel, exception, message, parameters);
-
-        var logMessagePublishedEvent = LogMessagePublished;
-        if (logMessagePublishedEvent != null)
+        public MqttNetLoggerWrapper(ILogger<MqttServer> logger)
         {
-            var logMessage = new MqttNetLogMessage
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public void Publish(MqttNetLogLevel level, string source, string message, object[] parameters, Exception exception)
+        {
+            var convertedLogLevel = ConvertLogLevel(level);
+            _logger.Log(convertedLogLevel, exception, message, parameters);
+
+            var logMessagePublishedEvent = LogMessagePublished;
+            if (logMessagePublishedEvent != null)
             {
-                Timestamp = DateTime.UtcNow,
-                ThreadId = Thread.CurrentThread.ManagedThreadId,
-                Source = source,
-                Level = level,
-                Message = message,
-                Exception = exception
-            };
+                var logMessage = new MqttNetLogMessage
+                {
+                    Timestamp = DateTime.UtcNow,
+                    ThreadId = Thread.CurrentThread.ManagedThreadId,
+                    Source = source,
+                    Level = level,
+                    Message = message,
+                    Exception = exception
+                };
 
-            logMessagePublishedEvent.Invoke(this, new MqttNetLogMessagePublishedEventArgs(logMessage));
+                logMessagePublishedEvent.Invoke(this, new MqttNetLogMessagePublishedEventArgs(logMessage));
+            }
         }
-    }
 
-    public bool IsEnabled => true;
+        public bool IsEnabled => true;
 
-    public event EventHandler<MqttNetLogMessagePublishedEventArgs> LogMessagePublished;
+        public event EventHandler<MqttNetLogMessagePublishedEventArgs> LogMessagePublished;
 
-    public void Publish(MqttNetLogLevel logLevel, string message, object[] parameters, Exception exception)
-    {
-        Publish(logLevel, null, message, parameters, exception);
-    }
-
-    private static LogLevel ConvertLogLevel(MqttNetLogLevel logLevel)
-    {
-        switch (logLevel)
+        public void Publish(MqttNetLogLevel logLevel, string message, object[] parameters, Exception exception)
         {
-            case MqttNetLogLevel.Error: return LogLevel.Error;
-            case MqttNetLogLevel.Warning: return LogLevel.Warning;
-            case MqttNetLogLevel.Info: return LogLevel.Information;
-            case MqttNetLogLevel.Verbose: return LogLevel.Trace;
+            Publish(logLevel, null, message, parameters, exception);
         }
 
-        return LogLevel.Debug;
+        private static LogLevel ConvertLogLevel(MqttNetLogLevel logLevel)
+        {
+            switch (logLevel)
+            {
+                case MqttNetLogLevel.Error: return LogLevel.Error;
+                case MqttNetLogLevel.Warning: return LogLevel.Warning;
+                case MqttNetLogLevel.Info: return LogLevel.Information;
+                case MqttNetLogLevel.Verbose: return LogLevel.Trace;
+            }
+
+            return LogLevel.Debug;
+        }
     }
 }

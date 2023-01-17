e36b87386f152aeef310ac8e994898649033a335
marcolewandowski <lewandowski@swms.de>
2023-01-17 10:24:26 +0100

    influx service added

---
+++ Source/InfluxDB/InfluxDbService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace MQTTnet.Server.InfluxDB
+{
+
+    public sealed class InfluxDbService
+    {
+        private static readonly Lazy<InfluxDbService> lazy = new Lazy<InfluxDbService>(() => new InfluxDbService());
+        private IConfigurationRoot config;
+
+        public static InfluxDbService Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+
+        private InfluxDbService()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+
+            config = builder.Build();
+
+            Connect();
+        }
+
+        private void Connect()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
